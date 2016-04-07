c38b9c0738a51be4ef6e99e832b7046a23e446b6
Shelmo <shelmobaches@outlook.com>
2016-04-06 21:39:15 -0300

    Arquivos Iniciais

--- Producao_Leite/src/DAO/AnimalDAO.java
+++ Producao_Leite/src/DAO/AnimalDAO.java
@@ -1,6 +1,8 @@
 package DAO;
 
+import util.Conecta;
 import Model.Animal;
+import java.sql.SQLException;
 /**
  *
  * @author Shelmo
@@ -9,6 +11,18 @@ public class AnimalDAO
 {
     public void registrarAnimal(Animal animal)
     {
+        Conecta c = new Conecta();
+        String sql = "INSERT INTO Animal VALUES (NULL, ?, ?, ?, ?, ?, ?)";
+        c.pStmt(sql);
         
+        try
+        {
+            c.getPreparedStatement().setString(1, animal.getNome());
+        }
+        catch (SQLException ex)
+        {
+            c.Desconectar();
+            System.err.println("Erro: " + ex.getMessage());
+        }
     }
 }
